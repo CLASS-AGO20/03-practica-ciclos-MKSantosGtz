e26ff77f93ab72ae8778719abf31bb792e1bdad9
Miguel Santos <msantos11@ucol.mx>
2020-12-09 16:26:44 -0600

    implemented factorial

--- src/main.js
+++ src/main.js
@@ -1,3 +1,25 @@
-export default class App {}
+export default class App {
+  factorial(numero) {
+    let resultado = numero;
+
+    for (let i = numero - 1; i >= 1; i--) {
+      resultado = resultado * i;
+    }
+
+    return resultado;
+  }
+  convertirAString(numero) {
+    let i = numero;
+    let asterisco = "*";
+    while (i <= numero) {
+      asterisco++;
+    }
+    return asterisco;
+  }
+}
 
 let app = new App();
+
+console.log(app.factorial(5));
+
+console.log(app.convertirAString(6));
